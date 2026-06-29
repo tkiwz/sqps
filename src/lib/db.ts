40d83c8758e267e37f28666be3f8bb73d7e26967
tkiwz <tkiwz@users.noreply.github.com>
2026-06-29 19:41:28 +0400

    Initial commit with Supabase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
-  })
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    __internal: {
+      engine: {
+        connectionLimit: 3, // ضروري لمنع انهيار السيرفر
+      },
+    } as any,
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
